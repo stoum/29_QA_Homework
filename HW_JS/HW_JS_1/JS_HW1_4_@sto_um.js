794feaa0c9bd6d228989b404dc95996b6b29251f
stoum <filinpok@gmail.com>
2022-04-24 13:12:27 +0300

    HW_29_QA

---
+++ HW_JS/HW_JS_1/JS_HW1_4_@sto_um.js
@@ -0,0 +1,30 @@
+
+
+const checkAge = function(age) {
+  
+       age = Number(age)
+   
+    if (isNaN(age)) {
+        console.log ("Error! Enter Number!")
+        return
+    } else {
+        
+        let age_2 = 18
+        let age_3 = 60
+    
+        if (age < age_2) {
+            console.log("You don’t have access cause your age is " + age + ". It’s less then " + age_2)
+    
+        } else if (age >= age_2 && age <= age_3) {
+           console.log("Welcome !")
+    
+        } else if (age > age_3) {
+            console.log("Keep calm and look Culture channel")
+    
+        } else {
+            console.log("Technical work") 
+        }
+    } 
+    }
+    let age = prompt("How old are you?", "Enter here");
+    checkAge(age)
