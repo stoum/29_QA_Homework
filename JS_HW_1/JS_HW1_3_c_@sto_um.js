75df196a1446027b754c93c8168c3cd5c913aa98
stoum <filinpok@gmail.com>
2022-04-25 22:17:32 +0300

    Homework_1_edit

---
+++ JS_HW_1/JS_HW1_3_c_@sto_um.js
@@ -0,0 +1,41 @@
+
+const checkAge = function(age) {
+   
+    if (/^\d+$/.test(age)){
+            
+        let age_2 = 18
+        let age_3 = 60
+    
+        if (age < age_2) {
+            
+            console.log("You don’t have access cause your age is " + age + ". It’s less then " + age_2)
+    
+        } else if (age >= age_2 && age < age_3) {
+           
+            console.log("Welcome !")
+    
+        } else if (age > age_3) {
+            
+            console.log("Keep calm and look Culture channel")
+    
+        } else {
+            
+            console.log("Technical work") 
+        }
+    } else {
+
+      console.log ("Error! Enter Number!")
+ 
+    }
+} 
+    
+    checkAge(true)
+    checkAge(5)
+    checkAge(25)
+    checkAge(62)
+    checkAge("25")
+    checkAge("25jk")
+    checkAge("yr")
+    checkAge("")
+    checkAge(25,5)   
+    checkAge("25.5")
