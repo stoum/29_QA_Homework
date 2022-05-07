ed87af7328ba6d520b283634e93cad81592ea91f
stoum <filinpok@gmail.com>
2022-05-07 21:05:14 +0300

    Homework_3_upd

---
+++ JS_HW_3/JS_HW3_2b_@sto_um.js
@@ -0,0 +1,10 @@
+
+ //Реализуйте считывание JSONки из файла task2.json с помощью, например, модуля fs. для дальнейшего
+ //использования в функции, описанной в задании
+
+const fs = require('fs');
+const users = JSON.parse(fs.readFileSync("./task2.json")) 
+
+let uniques = Array.from(new Set(users.map(item => JSON.stringify(item)))).map(item => JSON.parse(item))
+
+console.log(uniques)
