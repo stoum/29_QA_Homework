c9792346c2f895f96984482085c381f110e6084e
stoum <97801604+stoum@users.noreply.github.com>
2022-09-25 23:45:25 +0300

    Update St_Task_11.js

--- Other JS tasks/Stepik/Tasks for cycles/St_Task_11.js
+++ Other JS tasks/Stepik/Tasks for cycles/St_Task_11.js
@@ -1,7 +1,7 @@
 function testCycle(n) {
     
     for (i = 1; i<n; i++) {
-           if (i!=1 & i!=n & (n % i == 0)) {
+           if (i!=1 && i!=n && (n % i == 0)) {
            x = "Составное число"
            break
            } else {
